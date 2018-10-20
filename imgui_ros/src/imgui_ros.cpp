72b5b8ddf64e78b761a3eaadcdf43018f764b43f
Lucas Walter <wsacul@gmail.com>
2018-10-19 22:01:33 -0700

    Got rid of extra windows

--- imgui_ros/src/imgui_ros.cpp
+++ imgui_ros/src/imgui_ros.cpp
@@ -64,9 +64,6 @@ struct RosImage : public GlImage {
     dirty_ = true;
   }
 
-  ros::Subscriber sub_;
-  sensor_msgs::ImageConstPtr image_;
-
   // TODO(lucasw) factor this into a generic opengl function to put in parent class
   // if the image changes need to call this
   virtual bool updateTexture() {
@@ -146,6 +143,10 @@ struct RosImage : public GlImage {
     }
     ImGui::End();
   }
+
+private:
+  ros::Subscriber sub_;
+  sensor_msgs::ImageConstPtr image_;
 };  // RosImage
 
 struct CvImage : public GlImage {
@@ -372,39 +373,10 @@ private:
     ImGui_ImplSDL2_NewFrame(window);
     ImGui::NewFrame();
 
-    // 1. Show the big demo window (Most of the sample code is in
-    // ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear
-    // ImGui!).
-    if (show_demo_window)
-      ImGui::ShowDemoWindow(&show_demo_window);
-
-    // 2. Show a simple window that we create ourselves. We use a Begin/End pair
-    // to created a named window.
     {
-      static float f = 0.0f;
-      static int counter = 0;
 
-      ImGui::Begin("Hello, world!"); // Create a window called "Hello, world!"
-                                     // and append into it.
-
-      ImGui::Text("This is some useful text."); // Display some text (you can
-                                                // use a format strings too)
-      ImGui::Checkbox(
-          "Demo Window",
-          &show_demo_window); // Edit bools storing our window open/close state
-      ImGui::Checkbox("Another Window", &show_another_window);
-
-      ImGui::SliderFloat("float", &f, 0.0f,
-                         1.0f); // Edit 1 float using a slider from 0.0f to 1.0f
-      ImGui::ColorEdit3(
-          "clear color",
-          (float *)&clear_color); // Edit 3 floats representing a color
-
-      if (ImGui::Button("Button")) // Buttons return true when clicked (most
-                                   // widgets return true when edited/activated)
-        counter++;
-      ImGui::SameLine();
-      ImGui::Text("counter = %d", counter);
+      ImGui::Begin("stats"); // Create a window called "stats"
+                             // and append into it.
 
       ImGui::Text("Application average %.3f ms/frame (%.1f FPS)",
                   1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
@@ -413,7 +385,6 @@ private:
       for (size_t i = 0; i < images_.size(); ++i) {
         images_[i]->draw();
       }
-
     }
 
     // Rendering
