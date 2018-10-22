d4cfbf01ef294f04472f42161444162eda7948a5
Lucas Walter <wsacul@gmail.com>
2018-10-22 15:53:08 -0700

    Trying to get parameter value but not sure how- the changed_parameter type seems to be dds vendor specific #4

--- imgui_ros2/src/param_to_topic.cpp
+++ imgui_ros2/src/param_to_topic.cpp
@@ -20,7 +20,7 @@
 #include "rclcpp/rclcpp.hpp"
 #include "rcutils/cmdline_parser.h"
 
-#include "std_msgs/msg/string.hpp"
+#include "std_msgs/msg/float32.hpp"
 
 using namespace std::chrono_literals;
 
@@ -46,12 +46,47 @@ public:
   // can't init in constructor because shared_from_this will throw bad_weak_ptr
   void init()
   {
-    msg_ = std::make_shared<std_msgs::msg::String>();
+    msg_ = std::make_shared<std_msgs::msg::Float32>();
     const std::string topic_name = "test";
 
+    // Typically a parameter client is created for a remote node by passing the name of the remote
+    // node in the constructor; in this example we create a parameter client for this node itself.
+    parameters_client_ = std::make_shared<rclcpp::AsyncParametersClient>(this);
+
+    auto on_parameter_event_callback =
+      [this](const rcl_interfaces::msg::ParameterEvent::SharedPtr event) -> void
+      {
+        // TODO(wjwwood): The message should have an operator<<, which would replace all of this.
+        std::stringstream ss;
+        ss << "\nParameter event:\n new parameters:";
+        for (auto & new_parameter : event->new_parameters) {
+          ss << "\n  " << new_parameter.name;
+        }
+        ss << "\n changed parameters:";
+        for (auto & changed_parameter : event->changed_parameters) {
+          ss << "\n  " << changed_parameter.name;
+          if (changed_parameter.name == "foo") {
+            // TODO(lucasw) how to get parameter value?
+            // for (auto & parameter : parameters_client_->get_parameters({"foo"})) {
+            //   msg_->data = parameter.as_double();
+            // }
+            // msg_->data = changed_parameter.value;
+          }
+        }
+        ss << "\n deleted parameters:";
+        for (auto & deleted_parameter : event->deleted_parameters) {
+          ss << "\n  " << deleted_parameter.name;
+        }
+        ss << "\n";
+        RCLCPP_INFO(this->get_logger(), ss.str().c_str());
+      };
+
+    // Setup callback for changes to parameters.
+    parameter_event_sub_ = parameters_client_->on_parameter_event(on_parameter_event_callback);
+
     // Node already has std::enable_shared_from_this
-    auto parameters_client = std::make_shared<rclcpp::SyncParametersClient>(shared_from_this());
-    while (!parameters_client->wait_for_service(1s)) {
+    parameters_client_ = std::make_shared<rclcpp::AsyncParametersClient>(shared_from_this());
+    while (!parameters_client_->wait_for_service(1s)) {
       if (!rclcpp::ok()) {
         RCLCPP_ERROR(get_logger(), "Interrupted while waiting for the service. Exiting.");
         return;
@@ -60,18 +95,24 @@ public:
     }
 
     // TODO(lucasw) there ought to be a set_parameter for setting just one parameter
-    auto results = parameters_client->set_parameters({rclcpp::Parameter("foo", 2)});
+    #if 0
+    auto results = parameters_client_->set_parameters({rclcpp::Parameter("foo", 2.0)});
     for (auto result : results) {
       if (!result.successful) {
         RCLCPP_ERROR(get_logger(), "Failed to set parameter: %s", result.reason.c_str());
       }
     }
+    #endif
 
     // Create a function for when messages are to be sent.
     auto publish_message =
       [this]() -> void
       {
-        msg_->data = "test";
+        #if 0
+        for (auto & parameter : parameters_client_->get_parameters({"foo"})) {
+          msg_->data = parameter.as_double();
+        }
+        #endif
         // + std::to_string(count_++);
         // RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", msg_->data.c_str());
 
@@ -83,15 +124,16 @@ public:
     // Create a publisher with a custom Quality of Service profile.
     rmw_qos_profile_t custom_qos_profile = rmw_qos_profile_default;
     custom_qos_profile.depth = 7;
-    pub_ = this->create_publisher<std_msgs::msg::String>(topic_name, custom_qos_profile);
-
+    pub_ = this->create_publisher<std_msgs::msg::Float32>(topic_name, custom_qos_profile);
     // Use a timer to schedule periodic message publishing.
     timer_ = this->create_wall_timer(1s, publish_message);
   }
 
 private:
-  std::shared_ptr<std_msgs::msg::String> msg_;
-  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr pub_;
+  std::shared_ptr<rclcpp::AsyncParametersClient> parameters_client_;
+  rclcpp::Subscription<rcl_interfaces::msg::ParameterEvent>::SharedPtr parameter_event_sub_;
+  std::shared_ptr<std_msgs::msg::Float32> msg_;
+  rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr pub_;
   rclcpp::TimerBase::SharedPtr timer_;
 };
 
