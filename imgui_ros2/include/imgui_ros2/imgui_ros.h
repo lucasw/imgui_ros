80d557e6dc932bc9e7bf5c900d317cb2b53cd47b
Lucas Walter <wsacul@gmail.com>
2018-10-27 07:23:55 -0700

    Now buliding with colcon, no service to create windows yet.  The imgui window appears and shows the update rate #4

--- imgui_ros2/include/imgui_ros2/imgui_ros.h
+++ imgui_ros2/include/imgui_ros2/imgui_ros.h
@@ -28,25 +28,47 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef IMGUI_ROS_WINDOW_H
-#define IMGUI_ROS_WINDOW_H
-
 #include "imgui.h"
-// #include <imgui_ros/Window.h>
+// #include "imgui_impl_opengl3.h"
+// #include "imgui_impl_sdl.h"
+#include <imgui_ros2/image.h>
+// #include <imgui_ros/dynamic_reconfigure.h>
+#include <map>
 #include <mutex>
-#include <ros/ros.h>
+#include <opencv2/core.hpp>
+#include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/image.hpp>
+#include <SDL.h>
 
-struct Window {
-  Window(const std::string name, const std::string topic) :
-      name_(name), topic_(topic) {}
-  ~Window() {}
-  virtual void draw() = 0;
-protected:
-  // TODO(lucasw) or NULL or -1?
-  bool dirty_ = true;
-  std::string name_ = "";
-  std::string topic_ = "";
+
+namespace imgui_ros2 {
+class ImguiRos : public rclcpp::Node {
+public:
+  ImguiRos();
+  ~ImguiRos();
+
+private:
+  // bool addWindow(imgui_ros::srv::AddWindow::Request& req,
+  //                imgui_ros::srv::AddWindow::Response& res);
+  void update();
+
+  // Need to init the opengl context in same thread as the update
+  // is run in, not necessarily the same thread onInit runs in
+  void glInit();
   std::mutex mutex_;
+  bool init_;
+  // TODO(lucasw) std::shared_ptr
+  SDL_Window *window;
+  ImGuiIO io;
+  SDL_GLContext gl_context;
+  ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+  std::map<std::string, std::shared_ptr<Window> > windows_;
+
+  // TODO(lucasw) still need to update even if ros time is paused
+  rclcpp::TimerBase::SharedPtr update_timer_;
+
+  // ros::ServiceServer add_window_;
 };
 
-#endif  // IMGUI_ROS_IMAGE_H
+}  // namespace imgui_ros
