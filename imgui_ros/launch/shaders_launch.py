067eec1ec664e9f36cb868295e217c233daefd99
Lucas Walter <wsacul@gmail.com>
2019-01-25 06:31:57 -0800

    Allow shaders launch to be run in namespace

--- imgui_ros/launch/shaders_launch.py
+++ imgui_ros/launch/shaders_launch.py
@@ -13,12 +13,15 @@ def generate_launch_description():
     if not os.path.exists(prefix):
         os.makedirs(prefix)
 
+    namespace = launch.substitutions.LaunchConfiguration('namespace', default='')
+
     shader_dir = get_package_share_directory('imgui_ros') + '/../../lib/imgui_ros/'
     vertex_filename = shader_dir + 'vertex.glsl'
     fragment_filename = shader_dir + 'fragment.glsl'
     add_default_shaders = launch_ros.actions.Node(
             package='imgui_ros', node_executable='add_shaders.py',
-            node_name='add_shaders',
+            node_name='add_default_shaders',
+            node_namespace=[namespace],
             output='screen',
             arguments=['-n', 'default', '-v', vertex_filename, '-f', fragment_filename],
             )
@@ -27,6 +30,7 @@ def generate_launch_description():
     add_depth_shaders = launch_ros.actions.Node(
             package='imgui_ros', node_executable='add_shaders.py',
             node_name='add_depth_shaders',
+            node_namespace=[namespace],
             output='screen',
             arguments=['-n', 'depth', '-v', vertex_filename, '-f', fragment_filename],
             )
@@ -35,11 +39,16 @@ def generate_launch_description():
     add_cube_camera_shaders = launch_ros.actions.Node(
             package='imgui_ros', node_executable='add_shaders.py',
             node_name='add_cube_camera_shaders',
+            node_namespace=[namespace],
             output='screen',
             arguments=['-n', 'cube_map', '-v', vertex_filename, '-f', fragment_filename],
             )
 
     return launch.LaunchDescription([
+        launch.actions.DeclareLaunchArgument(
+            'namespace',
+            default_value='',  # TODO(lwalter) redundant with default above?
+            description='namespace to run in'),
         add_default_shaders,
         add_depth_shaders,
         add_cube_camera_shaders,
