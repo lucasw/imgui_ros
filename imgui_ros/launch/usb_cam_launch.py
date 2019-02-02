93cbd5b2cdbb28c51d8f7eab585c81af2611f0cd
Lucas Walter <wsacul@gmail.com>
2019-02-02 11:13:16 -0800

    Move composed nodes down into subdir, build optionally a usb_cam viewer node but it needs a launch file and script to setup the image viewer #68

---
+++ imgui_ros/launch/usb_cam_launch.py
@@ -0,0 +1,5 @@
+# Copyright Lucas Walter 2019
+# Requires internal_pub_sub branch of usb_cam https://github.com/lucasw/usb_cam/tree/internal_pub_sub
+# git clone -b internal_pub_sub https://github.com/lucasw/usb_cam
+
+
